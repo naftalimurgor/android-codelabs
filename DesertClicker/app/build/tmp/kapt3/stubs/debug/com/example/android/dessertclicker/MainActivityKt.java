ea97b84bc228fb5fb2cea359f60535f4906bff11
naftalimurgor <murgornaftali@gmailc.om>
2023-08-05 22:39:26 +0300

    activity lifecycle methods

---
+++ DesertClicker/app/build/tmp/kapt3/stubs/debug/com/example/android/dessertclicker/MainActivityKt.java
@@ -0,0 +1,9 @@
+package com.example.android.dessertclicker;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\b\n\u0000\n\u0002\u0010\u000e\n\u0000\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0002"}, d2 = {"tag", "", "app_debug"})
+public final class MainActivityKt {
+    @org.jetbrains.annotations.NotNull
+    public static final java.lang.String tag = "MainActivity";
+}
