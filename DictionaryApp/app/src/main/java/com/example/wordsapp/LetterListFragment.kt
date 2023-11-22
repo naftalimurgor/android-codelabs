7537521aaf34b5fbc749c8bb6290973bb7858f5d
naftalimurgor <murgornaftali@gmail.com>
2023-11-22 17:58:35 +0300

    wip: First fragment

--- DictionaryApp/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ DictionaryApp/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -1,6 +1,9 @@
 package com.example.wordsapp
 import androidx.fragment.app.Fragment
+import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 class LetterListFragment : Fragment() {
-
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
+    
 }
